7332fa6382ba13e34b24a7a31fb86cd509a5ffe6
Josh Elias <josh.g.elias@pm.me>
2022-05-26 13:54:26 -0400

    started filtering out options when theyre selected

--- src/index.js
+++ src/index.js
@@ -43,5 +43,55 @@ for(const product of productList) {
         }
     }
 }
-console.log(productMap);
+console.log('productMap', productMap);
+
+
+
+
+var filterContainer = document.getElementById('filter-container');
+//console.log('filter container', filterContainer)
+var selectDict = {};
+
+
+var getOnChange = (field) => {
+    console.log('getting on change with field', field);
+    return e => {
+        if(!e.target.value) return;
+
+        // What other fields are allowed
+        //console.log(`looking up allowed values for: ${e.target.value}  in: ${field}`)
+        var allowedValues = productMap[field][e.target.value];
+        //console.log('allowed values', allowedValues);
+
+        // When a style, finish or species is clicked, target the other select inputs
+        for(const subField of productFields) {
+            if(subField === field) continue;
+
+            // Filter out options out of other lists
+            //console.log('targeting OTHER select input', selectDict[subField]);
+            for(const option of selectDict[subField]) {
+                if(!option.value) continue;
+                //console.log(option);
+
+                //look up the "field" being filtered by
+                if(allowedValues[subField].includes(option.value)) {
+                    console.log(`hiding ${option.value}`)
+                    option.setAttribute("hidden", "hidden");
+                    //option.disable = true;
+                }
+
+                // if (option.value)
+                //option.hidden = true
+            }
+        }
+        //console.log(`${field} filter was clicked with: ${e.target.value}`);
+    }
+}
+
+for(const field of productFields) {
+    selectDict[field] = document.getElementById("select-"+field);
+    //console.log('found select select input', selectDict[field]);
+    selectDict[field].onchange = getOnChange(field);
+    // Iterate over options and add event listener
+}
 
