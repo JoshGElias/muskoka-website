4476b97b23be44a387eefac6b3a30ae86c485d87
Daemon <josh@daemonweb.tools>
2022-07-11 15:12:58 -0400

    fixed navbar closing

---
+++ src/home.js
@@ -0,0 +1,12 @@
+(function(document) {
+
+  const navMenu = document.getElementsByClassName("nav-menu")[0];
+
+  setInterval(() => {
+    if(navMenu.hasAttribute('data-nav-menu-open')) {
+      document.getElementsByClassName("fs-slider_arrow-right")[0].click();
+    }
+    
+  }, 5000);
+
+})(document);
