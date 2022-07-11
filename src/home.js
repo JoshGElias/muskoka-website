0de70c4eba302419246be179642ec091169e8b55
Daemon <josh@daemonweb.tools>
2022-07-11 16:00:49 -0400

    looking pretty good. must recover box shadow

--- src/home.js
+++ src/home.js
@@ -3,7 +3,7 @@
   const navMenu = document.getElementsByClassName("nav-menu")[0];
 
   setInterval(() => {
-    if(navMenu.hasAttribute('data-nav-menu-open')) {
+    if(!navMenu.hasAttribute('data-nav-menu-open')) {
       document.getElementsByClassName("fs-slider_arrow-right")[0].click();
     }
     
